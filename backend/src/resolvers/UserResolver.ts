aa744f4755881a2197f8ce438b250659b22bacd2
Renato-FO <renatoordonho@gmail.com>
2022-03-25 22:37:53 -0300

    first commit

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,26 @@
+import {
+  Arg,
+  Mutation,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { User } from "../models/User";
+import crypto from "crypto";
+
+@Resolver()
+export class UserResover {
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg("name") name: string) {
+    const user = { id: crypto.randomUUID(), name: name };
+
+    this.data.push(user);
+    return user;
+  }
+}
